019dc5c174babdc8d1c7f8f3aa74cf2ad9218391
Kanghua <88917038+kanghua58@users.noreply.github.com>
2023-10-23 13:36:10 -0400

    added video-control button

--- apple_vision_pro/main.js
+++ apple_vision_pro/main.js
@@ -739,3 +739,40 @@ overlay.addEventListener("click", () => {
   searchContainer.classList.add("hide");
   overlay.classList.remove("show");
 });
+
+
+// video control
+
+document.addEventListener("DOMContentLoaded", function() {
+  const video = document.querySelector('.intro-video'); 
+  const btn = document.querySelector('button.on-off');
+  const img = document.querySelector('button.on-off img');
+  let isPlaying = video.paused ? false : true; 
+
+  btn.addEventListener("click", controlVideo);
+
+  function controlVideo() {
+    if (isPlaying) {
+      video.pause(); // pause video
+      img.setAttribute('src', './images/resume_icon.png');
+    } else {
+      video.play(); // resume video
+      img.setAttribute('src', './images/pause_icon.png');
+    }
+    isPlaying = !isPlaying;
+  }
+  
+  // function updateBtn() {
+  //   const imgSrc = isPlaying ? 'image/pause_icon.png' : 'image/resume_icon.png';
+  //   img.setAttribute('src', imgSrc);
+  //   isPlaying = !isPlaying;
+  // }
+    const progressIndicator = document.querySelector('.progress-indicator');
+    const totalLength = 2 * Math.PI * 22.5; // circumference of the circle
+  
+    video.addEventListener('timeupdate', function() {
+      const percentage = video.currentTime / video.duration;
+      const offset = totalLength * (1 - percentage);
+      progressIndicator.style.strokeDashoffset = offset;
+    });
+});
